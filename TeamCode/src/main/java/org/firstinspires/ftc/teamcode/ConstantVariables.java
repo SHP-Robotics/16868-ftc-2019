9703190adf1c207855733358d177dbfcad310860
lknox23 <56613012+lknox23@users.noreply.github.com>
2019-10-22 12:39:10 -0700

    Added main stuff
    Added BaseRobot, ConstantVariables, MainTeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConstantVariables.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+public class ConstantVariables {
+    public static final double K_CLAW_SERVO_1_OPEN = 0;
+    public static final double K_CLAW_SERVO_1_CLOSED = 0.5;
+
+    public static final double K_CLAW_SERVO_2_OPEN = 0;
+    public static final double K_CLAW_SERVO_2_CLOSED = 0.5;
+
+    public static final int K_PPR_DRIVE = 1120;
+    public static final double K_DRIVE_WHEEL_DIA = 3;
+    public static final double K_DRIVE_DIA = 17;
+
+    public static final double K_DRIVE_WHEEL_CIRCUMFERENCE = K_DRIVE_WHEEL_DIA * Math.PI;
+    public static final double K_PPIN_DRIVE = K_PPR_DRIVE / K_DRIVE_WHEEL_CIRCUMFERENCE;
+
+    public static final double K_TURN_CIRCUMFERENCE = K_DRIVE_DIA * Math.PI;
+    public static final double K_PPTURN_DRIVE = K_PPIN_DRIVE * K_TURN_CIRCUMFERENCE;
+    public static final double K_PPDEG_DRIVE = K_PPTURN_DRIVE / 360;
+
+    public static final double K_DRIVE_ERROR_P = 250; // higher = less sensitive
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+
+public class MainTeleOp extends BaseRobot {
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+    @Override
+    public void loop() {
+        super.loop();
+        //drive train
+        tankanum_drive(gamepad1.right_stick_y, gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+        //arm
+        if (gamepad1.dpad_up) {
+            arm1 (1);
+        } else if (gamepad1.dpad_down) {
+            arm1 (-1);
+        }
+        if (gamepad1.y) {
+            arm2 (1);
+        } else if (gamepad1.a) {
+            arm2 (-1);
+        }
+
+        //base mover
+        if(gamepad1.b) {
+            base_mover(1);
+        } else if (gamepad1.x) {
+            base_mover(-1);
+        }
+
+        //claw
+        if (gamepad1.left_bumper) {
+            set_claw_servo_1(ConstantVariables.K_CLAW_SERVO_1_CLOSED);
+            set_claw_servo_2(ConstantVariables.K_CLAW_SERVO_2_CLOSED);
+        } else if (gamepad1.right_bumper) {
+            set_claw_servo_1(ConstantVariables.K_CLAW_SERVO_1_OPEN);
+            set_claw_servo_2(ConstantVariables.K_CLAW_SERVO_2_OPEN);
+        }
+    }
+}
